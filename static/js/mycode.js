1aaee9cc4f5a8c3f41196d9904612f0d7b7278cd
KoukiNakano <ksw2070305@stu.o-hara.ac.jp>
2021-11-26 11:59:52 +0900

    11/26

--- static/js/mycode.js
+++ static/js/mycode.js
@@ -1,5 +1,5 @@
 Blockly.defineBlocksWithJsonArray(
-    [{
+  [{
     "type": "go_left",
     "message0": "左に進む",
     "inputsInline": true,
@@ -51,41 +51,40 @@ Blockly.defineBlocksWithJsonArray(
     "colour": 230,
     "tooltip": "",
     "helpUrl": ""
-  }]
-);
-Blockly.JavaScript['go_left'] = function(block) {
-    var code = 'console.log("左に進む");\n';
-    //var statements = Blockly.JavaScript.statementToCode(block, 'NAME');
-    //var code = 'alert("");\n'
-    return code;
-  };
-   
-  Blockly.JavaScript['go_right'] = function(block) {
-    var code = 'console.log("右に進む");\n';
-    //var statements = Blockly.JavaScript.statementToCode(block, 'NAME');
-    //var code = 'alert("");\n'
-    return code;
-  };
-   
-  Blockly.JavaScript['go_up'] = function(block) {
-    var code = 'console.log("上に進む");\n';
-    //var statements = Blockly.JavaScript.statementToCode(block, 'NAME');
-    //var code = 'alert("");\n'
-    return code;
-  };
-   
-  Blockly.JavaScript['go_down'] = function(block) {
-    var code = 'console.log("下に進む");\n';
-    //var warn = '下に進む';
-　  //alert(warn);
-    var statements = Blockly.JavaScript.statementToCode(block, 'NAME');
-    //var code = 'alert("");\n'
-    return code;
-  };
-  Blockly.JavaScript['test'] = function (block) {
-    var statements = Blockly.JavaScript.statementToCode(block, 'NAME');
-    //TODO: Assemble JavaScript into code variable.
-    var code = 'function hoge(){\n' + statements + '\n}\nhoge();';
-    //var code = 'alert("");\n'
-    return code;
-  };
+  }
+  ]);
+Blockly.JavaScript['go_left'] = function (block) {
+  var code = 'console.log("左に進む");\n';
+  //var statements = Blockly.JavaScript.statementToCode(block, 'NAME');
+  //var code = 'alert("");\n'
+  return code;
+};
+Blockly.JavaScript['go_right'] = function (block) {
+  var code = 'console.log("右に進む");\n';
+  //var statements = Blockly.JavaScript.statementToCode(block, 'NAME');
+  //var code = 'alert("");\n'
+  return code;
+};
+
+Blockly.JavaScript['go_up'] = function (block) {
+  var code = 'console.log("上に進む");\n';
+  //var statements = Blockly.JavaScript.statementToCode(block, 'NAME');
+  //var code = 'alert("");\n'
+  return code;
+};
+
+Blockly.JavaScript['go_down'] = function (block) {
+  var code = 'console.log("下に進む");\n';
+  //var warn = '下に進む';
+  //alert(warn);
+  var statements = Blockly.JavaScript.statementToCode(block, 'NAME');
+  //var code = 'alert("");\n'
+  return code;
+};
+Blockly.JavaScript['test'] = function (block) {
+  var statements = Blockly.JavaScript.statementToCode(block, 'NAME');
+  //TODO: Assemble JavaScript into code variable.
+  var code = 'function hoge(){\n' + statements + '\n}\nhoge();';
+  //var code = 'alert("");\n'
+  return code;
+};
